f680e04b40ec07b4e03e4aaf4e41d87c64b53974
Nathan Lilienthal <nathan@nixpulvis.com>
2018-06-09 21:45:23 -0400

    pkgify

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "parser-combinator")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+#| (define scribblings '(("scribblings/parser-combinator.scrbl" ()))) |#
+(define pkg-desc "TODO")
+(define version "0.1")
+(define pkg-authors '(nixpulvis))
